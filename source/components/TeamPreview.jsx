c858ef74d6184e0baaade1e9c60b74e564a36d6f
Karthikeyan <karthikeyan@auctionsoftware.com>
2020-07-11 14:39:45 +0530

    missing components

---
+++ source/components/TeamPreview.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Alert } from "react-bootstrap";
+
+export default class TeamPreview extends Component {
+  render() {
+    const { item, onClose, index } = this.props;
+    return (
+      <Alert bsStyle="success" onDismiss={() => onClose(index)} key={index}>
+        <div>
+          {item.text_team}, Worker Count: {item.workerCount}, In Time:{" "}
+          {item.inTime}-Out Time:{item.outTime}
+        </div>
+      </Alert>
+    );
+  }
+}
